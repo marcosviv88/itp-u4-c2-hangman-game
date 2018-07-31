6b1f66bd4267a377f046386c116facf531b004b8
Marcos Vivanco <marcosviv88@gmail.com>
2018-07-31 21:19:51 +0000

    done!

--- hangman/game.py
+++ hangman/game.py
@@ -1,25 +1,120 @@
 from .exceptions import *
+import random
 
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
+# LIST_OF_WORDS = []
 
+big_string = '''At eight o'clock Kutuzov rode to Pratz at the head of Miloradovich's fourth column, the one 
+which was to take the place of the columns of Przebyszewski and Langeron, which had already gone 
+down. He greeted the men of the head regiment and gave the order to move, thus showing that he intended to 
+lead the column himself. Having ridden to the village of Pratz, he halted. Prince Andrei, one of the enormous 
+number of persons constituting the commander in chief's suite, stood behind him. Prince Andrei felt excited, irritated, 
+and at the same time restrainedly calm, as a man usually is when a long-desired moment comes. He was firmly convinced 
+that this was the day of his Toulon or his bridge of Arcole. How it would happen, he did not know, but 
+he was firmly convinced that it would be so. The locality and position of our troops were known to him, as 
+far as they could be known to anyone in our army. His own strategic plan, which there obviously could be no thought
+of carrying out now, was forgotten. Now, entering into Weyrother's plan, Prince Andrei pondered the possible
+happenstances and came up with new considerations, such as might call for his swiftness of reflection and decisiveness.'''
+
+def slice_string(string):
+    list_of_words = []
+    sliced_string = string.split()
+    for word in sliced_string:
+        if ',' not in word and '.' not in word:
+            list_of_words.append(word)
+        
+    return list_of_words
+
+# print(slice_string(big_string))
+# Output of function over big_string:
+
+LIST_OF_WORDS = ['At', 'eight', "o'clock", 'Kutuzov', 'rode', 'to', 'Pratz', 'at', 'the', 'head', 'of', "Miloradovich's", 
+'fourth', 'the', 'one', 'which', 'was', 'to', 'take', 'the', 'place', 'of', 'the', 'columns', 'of', 'Przebyszewski', 'and', 
+'which', 'had', 'already', 'gone', 'He', 'greeted', 'the', 'men', 'of', 'the', 'head', 'regiment', 'and', 'gave', 'the', 'order', 
+'to', 'thus', 'showing', 'that', 'he', 'intended', 'to', 'lead', 'the', 'column', 'Having', 'ridden', 'to', 'the', 'village', 
+'of', 'he', 'Prince', 'one', 'of', 'the', 'enormous', 'number', 'of', 'persons', 'constituting', 'the', 'commander', 'in', "chief's", 
+'stood', 'behind', 'Prince', 'Andrei', 'felt', 'and', 'at', 'the', 'same', 'time', 'restrainedly', 'as', 'a', 'man', 
+'usually', 'is', 'when', 'a', 'long-desired', 'moment', 'He', 'was', 'firmly', 'convinced', 'that', 'this', 'was', 'the', 
+'day', 'of', 'his', 'Toulon', 'or', 'his', 'bridge', 'of', 'How', 'it', 'would', 'he', 'did', 'not', 'but', 'he', 'was', 
+'firmly', 'convinced', 'that', 'it', 'would', 'be', 'The', 'locality', 'and', 'position', 'of', 'our', 'troops', 'were', 
+'known', 'to', 'as', 'far', 'as', 'they', 'could', 'be', 'known', 'to', 'anyone', 'in', 'our', 'His', 'own', 'strategic', 
+'which', 'there', 'obviously', 'could', 'be', 'no', 'thought', 'of', 'carrying', 'out', 'was', 'entering', 'into', "Weyrother's",
+'Prince', 'Andrei', 'pondered', 'the', 'possible', 'happenstances', 'and', 'came', 'up', 'with', 'new', 'such', 'as', 
+'might', 'call', 'for', 'his', 'swiftness', 'of', 'reflection', 'and']
 
 def _get_random_word(list_of_words):
-    pass
-
+    if not list_of_words:
+        raise InvalidListOfWordsException(Exception)
+    return random.choice(list_of_words)
 
 def _mask_word(word):
-    pass
+    if not word:
+        raise InvalidWordException(Exception)
+    masked = ('*' * len(word))
+    return masked
+    
+# print(_mask_word('hell'))    
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
-
-
+    n_word = ''
+    if not answer_word:
+        raise InvalidWordException(Exception)
+    if len(character) > 1:
+        raise InvalidGuessedLetterException(Exception)
+    if len(answer_word) != len(masked_word):
+        raise InvalidWordException(Exception)
+    for i in range(len(answer_word)): 
+        if answer_word[i].lower() == character.lower(): # answer_word[i]? answer_word at position/index i?
+            n_word += character.lower()
+        else:
+            n_word += masked_word[i]
+    return n_word
+    
 def guess_letter(game, letter):
-    pass
-
-
+    """
+    You don't need an if statement to check that, you are just reading the game state
+    from game.
+    Ok they're trying to guess a letter, I should check to see if that letter is in the answer word
+    I need to check the game state to find that out.
+    Oh it is in the answer word, I need to uncover that in the masked word then, again modify game state
+    It wasn't a miss so I don't need to modify remaining misses, but I do have to update previous guesses
+    again modify game state.
+    game is a dictionary, you need to access all that stuff from *in* the dictionary
+    
+    Just write the code to pass the test you're working on. You can build the logic up from there.
+    
+    Pseudo code:
+    if letter in answer:
+        unmask word
+    else:
+        modify remaining guesses
+    add letter to previous guesses
+    
+    game['answer_word']
+    ALL game state stuff is part of the game dictionary, it must be read from the dictionary and any
+    updates need to be made to the dictionary
+    
+    I need to go get my son up from his nap, I'll leave this to you for now.
+    """
+    # OK but I still don't get the concept of that logic you mention. 
+    # OK thks
+    if not '*' in game['masked_word']:
+            raise GameFinishedException
+    if letter.lower() in game['answer_word'].lower(): # need to lower both so comparing apples to apples
+        game['masked_word'] = _uncover_word(game['answer_word'], game['masked_word'], letter)
+        game['previous_guesses'].append(letter.lower())
+        if not '*' in game['masked_word']:
+            raise GameWonException
+        
+    else:
+        game['remaining_misses'] -= 1
+        game['previous_guesses'].append(letter.lower())
+        if game['remaining_misses'] == 0:
+            raise GameLostException
+    if game['remaining_misses'] == 0:
+        raise GameFinishedException
+        
 def start_new_game(list_of_words=None, number_of_guesses=5):
     if list_of_words is None:
         list_of_words = LIST_OF_WORDS
--- tests.py
+++ tests.py
@@ -3,45 +3,45 @@ from hangman.game import (
     start_new_game, guess_letter, _get_random_word, _mask_word, _uncover_word)
 from hangman.exceptions import *
 
-
+# passing
 def test_uncover_word_with_empty_word():
     """Words are empty"""
     with pytest.raises(InvalidWordException):
         _uncover_word('', '', 'x')
 
-
+# passing
 def test_uncover_word_with_invalid_character():
     """Character to guess has len() > 1"""
     with pytest.raises(InvalidGuessedLetterException):
         _uncover_word('aaa', '***', 'xyz')
 
-
+# passing
 def test_uncover_word_with_invalid_masked_word():
     """Length of words is different"""
     with pytest.raises(InvalidWordException):
         _uncover_word('aaa', '**********', 'x')
 
-
+# passing
 def test_uncover_word_with_correct_character():
     word = _uncover_word('Python', '******', 'y')
     assert word == '*y****'
 
-
+# passing
 def test_uncover_word_with_miss_character():
     word = _uncover_word('Python', '******', 'z')
     assert word == '******'
 
-
+# passing
 def test_uncover_word_with_repeated_elements():
     word = _uncover_word('rmotr', '*****', 'r')
     assert word == 'r***r'
 
-
+# passing
 def test_uncover_word_with_all_equal_characters():
     word = _uncover_word('aaa', '***', 'a')
     assert word == 'aaa'
 
-
+# passing
 def test_uncover_word_with_misses_and_guesses():
     word = _uncover_word('Python', '******', 'y')
     assert word == '*y****'
@@ -61,44 +61,44 @@ def test_uncover_word_with_misses_and_guesses():
     word = _uncover_word('Python', '*y**on', 'a')  # Miss!
     assert word == '*y**on'
 
-
+# passing
 def test_uncover_word_is_case_insensitive_same_case():
     word = _uncover_word('Python', '******', 'P')
     assert word == 'p*****'
 
-
+# passing
 def test_uncover_word_is_case_insensitive_different_case():
     word = _uncover_word('Python', '******', 'p')
     assert word == 'p*****'
 
-
+# passing
 def test_get_random_word_with_one_word():
     list_of_words = ['rmotr']
     word_to_guess = _get_random_word(list_of_words)
     assert word_to_guess == 'rmotr'
 
-
+# passing
 def test_get_random_word_with_many_words():
     list_of_words = ['rmotr', 'python', 'intro']
     word_to_guess = _get_random_word(list_of_words)
     assert word_to_guess in list_of_words
 
-
+# passing
 def test_get_random_word_with_empty_list():
     with pytest.raises(InvalidListOfWordsException):
         word_to_guess = _get_random_word([])
 
-
+# passing
 def test_mask_word_with_valid_word():
     masked = _mask_word('Python')
     assert masked == '******'
 
-
+# passing
 def test_mask_word_with_empty_string():
     with pytest.raises(InvalidWordException):
         masked = _mask_word('')
 
-
+# passing
 def test_start_new_game_initial_state():
     # This test verifies that you haven't changed start_new_game
     game = start_new_game(['Python'], number_of_guesses=3)
@@ -109,7 +109,7 @@ def test_start_new_game_initial_state():
         'remaining_misses': 3,
     }
 
-
+# passing
 def test_game_with_one_correct_guess():
     game = start_new_game(['Python'])
     guess_letter(game, 'y')
@@ -117,7 +117,7 @@ def test_game_with_one_correct_guess():
     assert game['remaining_misses'] == 5
     assert game['previous_guesses'] == ['y']
 
-
+# passing
 def test_game_with_two_correct_guesses_same_move():
     game = start_new_game(['rmotr'])
     guess_letter(game, 'r')
@@ -125,7 +125,7 @@ def test_game_with_two_correct_guesses_same_move():
     assert game['remaining_misses'] == 5
     assert game['previous_guesses'] == ['r']
 
-
+#  passing
 def test_game_with_one_incorrect_guess():
     game = start_new_game(['Python'])
 
@@ -134,7 +134,7 @@ def test_game_with_one_incorrect_guess():
     assert game['remaining_misses'] == 4  # (5 - 1)
     assert game['previous_guesses'] == ['x']
 
-
+# passing
 def test_game_with_several_incorrect_guesses():
     game = start_new_game(['Python'])
 
@@ -148,7 +148,7 @@ def test_game_with_several_incorrect_guesses():
     assert game['remaining_misses'] == 3
     assert game['previous_guesses'] == ['x', 'z']
 
-
+# passing
 def test_game_with_several_correct_guesses():
     game = start_new_game(['Python'])
 
@@ -167,7 +167,7 @@ def test_game_with_several_correct_guesses():
     assert game['remaining_misses'] == 5
     assert game['previous_guesses'] == ['y', 'o', 't']
 
-
+# passing
 def test_game_with_several_correct_and_incorrect_guesses():
     game = start_new_game(['Python'])
 
@@ -191,7 +191,7 @@ def test_game_with_several_correct_and_incorrect_guesses():
     assert game['remaining_misses'] == 3
     assert game['previous_guesses'] == ['y', 'x', 'o', 'z']
 
-
+# passing
 def test_guess_word_is_case_insensitve():
     game = start_new_game(['Python'])
 
@@ -205,7 +205,7 @@ def test_guess_word_is_case_insensitve():
     assert game['remaining_misses'] == 5
     assert game['previous_guesses'] == ['p', 'n']
 
-
+# passing
 def test_game_wins_first_try():
     game = start_new_game(['aaa'])
 
@@ -215,7 +215,7 @@ def test_game_wins_first_try():
         assert game['remaining_misses'] == 5
         assert game['previous_guesses'] == ['a']
 
-
+# passing 
 def test_game_loses_first_try():
     game = start_new_game(['Python'], number_of_guesses=1)
 
@@ -225,7 +225,7 @@ def test_game_loses_first_try():
         assert game['remaining_misses'] == 0
         assert game['previous_guesses'] == ['x']
 
-
+# passing
 def test_game_wins_several_moves_repeated_words():
     game = start_new_game(['aba'])
 
@@ -240,7 +240,7 @@ def test_game_wins_several_moves_repeated_words():
         assert game['remaining_misses'] == 5
         assert game['previous_guesses'] == ['a', 'b']
 
-
+# passing
 def test_game_wins_several_moves():
     game = start_new_game(['abc'])
 
@@ -260,7 +260,7 @@ def test_game_wins_several_moves():
         assert game['remaining_misses'] == 5
         assert game['previous_guesses'] == ['a', 'c', 'b']
 
-
+# passing
 def test_game_wins_several_moves_some_misses():
     game = start_new_game(['abc'])
 
@@ -290,7 +290,7 @@ def test_game_wins_several_moves_some_misses():
         assert game['remaining_misses'] == 3
         assert game['previous_guesses'] == ['a', 'x', 'c', 'z', 'b']
 
-
+# passing
 def test_game_loses_several_guesses():
     game = start_new_game(['Python'], number_of_guesses=3)
 
@@ -310,7 +310,7 @@ def test_game_loses_several_guesses():
         assert game['remaining_misses'] == 0
         assert game['previous_guesses'] == ['x', 'z', 'a']
 
-
+# passing
 def test_game_loses_with_some_correct_guesses():
     game = start_new_game(['Python'], number_of_guesses=3)
 
@@ -340,7 +340,7 @@ def test_game_loses_with_some_correct_guesses():
         assert game['remaining_misses'] == 0
         assert game['previous_guesses'] == ['y', 'x', 'z', 't', 'a']
 
-
+# passing
 def test_game_already_won_raises_game_finished():
     game = start_new_game(['aaa'])
 
@@ -353,7 +353,7 @@ def test_game_already_won_raises_game_finished():
     with pytest.raises(GameFinishedException):
         guess_letter(game, 'x')  # Doesn't matter
 
-
+# passing
 def test_game_already_lost_raises_game_finished():
     game = start_new_game(['Python'], number_of_guesses=1)
 
